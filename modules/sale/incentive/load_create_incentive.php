a237d5afa6fbe0d60821418bcfb686db30a11b1f
Bearjai0 <bearjai0@gmail.com>
2024-03-06 10:18:56 +0700

    Pre change query cost
    query cost by component on this behavior

--- modules/sale/incentive/load_create_incentive.php
+++ modules/sale/incentive/load_create_incentive.php
@@ -3,46 +3,39 @@
 
     $inc_month = isset($_POST['sendingTask']) ? $_POST['sendingTask'] : '';
 ?>
-<form id="_add_tooling" data-parsley-validate="true">
+<form id="_incenlist" data-parsley-validate="true">
     <div class="modal-dialog d-flex justify-content-center">
-        <div class="modal-content mb-5" style="width: 60%;">
+        <div class="modal-content mb-5" style="width: 95%;">
             <div class="modal-header">
-                <h4 class="modal-title">Incentive Management</h4>
+                <h4 class="modal-title">Incentive of <?=date('F Y', strtotime($inc_month))?></h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div>
             <div class="modal-body">
-                <small class="fw-600">Month / Year</small>
-                <input type="text" id="inc_month" name="inc_month" value="<?=date('F Y', strtotime($inc_month))?>" class="form-control" data-parsley-required="true" readonly>
-                
-                <h4 class="mb-0 mt-3">Revenue <?=date('F Y', strtotime($inc_month))?> of B2B</h4>
-                <table id="tbl_revenue_details" class="table table-bordered" style="width: 100%">
+                <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Customer</th>
-                            <th>Revenue</th>
-                            <th>Amount Cost 24%</th>
-                            <th>Margin (Bath)</th>
-                            <th>Margin %</th>
+                            <th nowrap class="bg-gradient-black text-white text-center" width="20%">Field Name</th>
+                            <th nowrap class="bg-gradient-black text-white text-center" width="80%">Field Value</th>
                         </tr>
                     </thead>
-                </table>
-                
-                <h4 class="mb-0 mt-4">Summary sale incentive</h4>
-                <table id="tbl_incetive_sale_list" class="table table-bordered" style="width: 100%">
-                    <thead>
+                    <tbody>
                         <tr>
-                            <th>#</th>
-                            <th>Position</th>
-                            <th>Sale Name</th>
-                            <th>Incentive Rate</th>
-                            <th>Total Incentive</th>
+                            <td class="pt-1 pb-1 bg-gray-200 text-end text-end">Incentive Month :</td>
+                            <td class="pt-1 pb-1"><input type="text" id="inc_month" name="inc_month" class="form__field p-0" value="<?=date('F Y', strtotime($inc_month))?>" data-parsley-required="true" readonly></td>
                         </tr>
-                    </thead>
+                        <tr>
+                            <td class="pt-1 pb-1 bg-gray-200 text-end">Choose income file :</td>
+                            <td class="pt-1 pb-1">
+                                <input type="file" id="upfile" name="upfile" class="form__field p-0" data-parsley-required="true">
+                                <button type="button" id="confirmUpfile" class="btn badge bg-gradient-blue text-white">Confirm</button>
+                            </td>
+                    </tbody>
                 </table>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Confirm</button>
-                <a href="javascript:;" class="btn btn-white" data-bs-dismiss="modal">Close</a>
+                <hr>
+                <center>
+                    <button type="submit" disabled class="btn bg-gradient-blue-indigo fw-600 text-white ps-5 pe-5">Confirm</button>
+                    <button type="button" data-bs-dismiss="modal" class="btn btn-white fw-600 ms-5 ps-5 pe-5">Close</button>
+                </center>
             </div>
         </div>
     </div>
@@ -50,51 +43,26 @@
 
 <script type="text/javascript">
     $(document).ready(function(data){
-        GetRevenue()
+    
     })
 
+    $("#confirmUpfile").click(function(){
+        var upfile = $("#upfile")[0].files[0]
+        var formData = new FormData()
+        formData.append('upfile', upfile)
+        formData.append('protocol', 'MatchFile')
 
-    function GetRevenue(){
-        $.post('<?=$CFG->fol_sale_incentive?>/management', { protocol: 'GetRevenue', inc_month: '<?=$inc_month?>' }, function(data){
-            try {
-                const result = JSON.parse(data)
-                console.log(result)
-
-                $('#tbl_revenue_details').DataTable({
-                    // scrollX   : true,
-                    searching : false,
-                    paging    : false,
-                    ordering  : false,
-                    info      : false,
-                    bDestroy  : true,
-                    data      : result.datas,
-                    columns : [
-                        { data: function(data){ return data.GroupCustomer }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(data.revenue, { seperator: ',', symbol: '', precision: 2 }).format() }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(data.cost, { seperator: ',', symbol: '', precision: 2 }).format() }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(data.margin, { seperator: ',', symbol: '', precision: 2 }).format() }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(((data.revenue - data.cost) / data.cost) * 100, { seperator: ',', symbol: '', precision: 2 }).format() + '%' }, className: "text-nowrap text-center" },
-                    ]
-                }).draw(false)
-
-                $('#tbl_incetive_sale_list').DataTable({
-                    searching : false,
-                    paging    : false,
-                    ordering  : false,
-                    info      : false,
-                    bDestroy  : true,
-                    data      : result.prilla,
-                    columns : [
-                        { data: function(data){ return data.list }, className: "text-nowrap text-center" },
-                        { data: function(data){ return data.user_position }, className: "text-nowrap text-center" },
-                        { data: function(data){ return data.user_name_en }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(data.inc_rate, { seperator: ',', symbol: '', precision: 2 }).format() }, className: "text-nowrap text-center" },
-                        { data: function(data){ return currency(data.inc_total, { seperator: ',', symbol: '', precision: 2 }).format() }, className: "text-nowrap text-center" },
-                    ]
-                }).draw(false)
-            } catch(e) {
-                SwalOnlyText('error', 'ไม่สามารถแสดงข้อมูล Revenue ได้ ' + e.message())
+        $.ajax({
+            url: 'management',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(resp) {
+                console.log('resp is ' + resp)
+            }, error: function(xhr, status, error) {
+                console.log('failed on upload')
             }
         })
-    }
+    })
 </script>
--- modules/sale/incentive/management.php
+++ modules/sale/incentive/management.php
@@ -1,159 +1,113 @@
 <?php
     require_once("../../../session.php");
+    require_once("../../../../library/PHPSpreadSheet/vendor/autoload.php");
+
+    use PhpOffice\PhpSpreadsheet\IOFactory;
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+    use PhpOffice\PhpSpreadsheet\Shared\Date;
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+    $reader->setReadDataOnly(true);
+    $reader->setReadEmptyCells(false);
 
     $protocol = isset($_POST['protocol']) ? $_POST['protocol'] : '';
     $inc_month = isset($_POST['inc_month']) ? date('Y-m', strtotime($_POST['inc_month'])) : '';
 
-    $json = [];
-    $pril = [];
-    $arr = [];
+    $column_head = ['Period', 'Invoice Number', 'Invoice Date', 'Invoice Type', 'Customer Name', 'Amount', 'VAT Amount', 'Total Amount', 'RV Number', 'RV Date', 'Project Name'];
+    $column_name = ['A','B','C','D','E','F','G','H','I','J','K'];
+    $revenue = 0;
+    $amount_cost = 0;
+    $margin = 0;
+    $ivd;
 
-    if($protocol == "GetRevenue"){
+    if($protocol == "MatchFile"){
         try {
-            // $MRev = $db_con->query(
-            //     "SELECT CASE
-            //                 WHEN inv_cus_code IN ('AAP', 'TKM', 'TIT', 'LAT') THEN inv_cus_code
-            //                 ELSE 'Included VMI'
-            //             END AS GroupCustomer,
-            //             SUM(con_res_qty * des_sell_per_pcs) AS revenue, 
-            //             SUM(cost_total_oh) AS cost,
-            //             SUM(con_res_qty * des_sell_per_pcs) - SUM(cost_total_oh) AS margin
-            //      FROM tbl_inv_detail_mst AS A
-            //      LEFT JOIN tbl_inv_mst AS B ON A.des_inv_no = B.inv_no
-            //      LEFT JOIN tbl_order_dtn_detail_mst AS C ON A.des_det_uniq = C.det_uniq
-            //      LEFT JOIN tbl_ffmc_reserve_order AS D ON C.det_ref_code = D.con_ref_code AND D.con_status = 'Picking'
-            //      LEFT JOIN tbl_fg_inven_mst AS E ON D.con_res_pallet_id = E.pallet_id
-            //      LEFT JOIN tbl_confirm_print_list AS F ON E.pallet_lot_no = F.list_conf_no
-            //      LEFT JOIN tbl_bom_mst AS G ON E.pallet_bom_uniq = G.bom_uniq
-            //      LEFT JOIN tbl_customer_mst AS H ON B.inv_cus_code = H.cus_code
-            //      WHERE B.inv_date LIKE '$inc_month%' AND A.des_status IN('INV','Post Invoice')
-            //      GROUP BY 
-            //         CASE
-            //             WHEN inv_cus_code IN ('AAP', 'TKM', 'TIT', 'LAT') THEN inv_cus_code
-            //             ELSE 'Included VMI'
-            //         END"
-            // );
+            $spreadsheet = $reader->load($_FILES['upfile']['tmp_name']);
+            // $data = $spreadsheet->getActiveSheet();
+            $data = $spreadsheet->setActiveSheetIndex(1);
+            $highestRow = intval($data->getHighestRow());
 
-            $MRev = $db_con->query(
-                "SELECT 'Included VMI' AS GroupCustomer,
-                        SUM(con_res_qty * des_sell_per_pcs) AS revenue, 
-                        SUM(con_res_qty * cost_total_oh) AS cost,
-                        SUM(con_res_qty * des_sell_per_pcs) - SUM(con_res_qty * cost_total_oh) AS margin
-                 FROM tbl_inv_detail_mst AS A
-                 LEFT JOIN tbl_inv_mst AS B ON A.des_inv_no = B.inv_no
-                 LEFT JOIN tbl_order_dtn_detail_mst AS C ON A.des_det_uniq = C.det_uniq
-                 LEFT JOIN tbl_ffmc_reserve_order AS D ON C.det_ref_code = D.con_ref_code AND D.con_status = 'Picking'
-                 LEFT JOIN tbl_fg_inven_mst AS E ON D.con_res_pallet_id = E.pallet_id
-                 LEFT JOIN tbl_confirm_print_list AS F ON E.pallet_lot_no = F.list_conf_no
-                 LEFT JOIN tbl_bom_mst AS G ON E.pallet_bom_uniq = G.bom_uniq
-                 LEFT JOIN tbl_customer_mst AS H ON B.inv_cus_code = H.cus_code
-                 WHERE B.inv_date LIKE '$inc_month%' AND A.des_status IN('INV','Post Invoice') AND inv_cus_code NOT IN('AAP1', 'TKM', 'TIT', 'LAT') AND H.cus_type IN('B2B','GDJM')"
-            );
-            $MResult = $MRev->fetch(PDO::FETCH_ASSOC);
-
-
-            //todo >>>>>>>>>>>>>>>>>>>> VMI Cost and Selling Details >>>>>>>>>>>>>>>>>>>>
-            //todo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            $VMev = $vmi_con->query(
-                "SELECT SUM(G.ps_t_tags_packing_std * det_sell_per_pcs) AS revenue,
-                        SUM(G.ps_t_tags_packing_std * J.bom_cost_per_pcs) AS cost,
-                        SUM(G.ps_t_tags_packing_std * det_sell_per_pcs) - SUM(G.ps_t_tags_packing_std * J.bom_cost_per_pcs) AS margin
-                 FROM tbl_inv_mst AS A
-                 LEFT JOIN tbl_inv_detail_mst AS B ON A.inv_no = B.det_inv_no
-                 LEFT JOIN tbl_dn_head AS E ON B.det_dn_h_id = E.dn_h_id
-                 LEFT JOIN tbl_dn_tail AS F ON B.det_dn_t_id = F.dn_t_id AND dn_t_status != 'Return'
-                 LEFT JOIN tbl_picking_tail AS G ON F.dn_t_picking_code = G.ps_t_picking_code
-                 LEFT JOIN tbl_receive AS H ON G.ps_t_tags_code = H.receive_tags_code
-                 INNER JOIN tbl_dn_usage_conf AS I ON B.det_dn_usage_id = I.dn_usage_id
-                                                 AND G.ps_t_picking_code = I.dn_picking_code
-                                                 AND G.ps_t_bom_uniq = I.dn_bom_uniq
-                                                 AND H.receive_repn_id = I.dn_repn_id
-                 LEFT JOIN tbl_bom_mst AS J ON I.dn_bom_uniq = J.bom_uniq
-                 LEFT JOIN tbl_fg_inven_mst AS K ON G.ps_t_pallet_code = K.pallet_code
-                 LEFT JOIN tbl_confirm_print_list AS L ON K.pallet_job_no = L.list_conf_no
-                 WHERE G.ps_t_status != 'Cancel' AND B.det_status != 'Cancel' AND B.det_issue_inv_from = 'GTN' AND A.inv_date LIKE '$inc_month%'"
-            ); //! Usage Confirm
-            $VMResult = $VMev->fetch(PDO::FETCH_ASSOC);
-            
-            $MResult['revenue'] += $VMResult['revenue'];
-            $MResult['cost'] += $VMResult['cost'];
-            $MResult['margin'] += $VMResult['margin'];
-            array_push($json, $MResult);
-            
-
-            $MIcl = $db_con->query(
-                "SELECT inv_cus_code AS GroupCustomer,
-                        SUM(con_res_qty * des_sell_per_pcs) AS revenue, 
-                        SUM(con_res_qty * cost_total_oh) + (SUM(con_res_qty * des_sell_per_pcs) * cus_cff_ratio) AS cost,
-                        SUM(con_res_qty * des_sell_per_pcs) - (SUM(con_res_qty * cost_total_oh) + (SUM(con_res_qty * des_sell_per_pcs) * cus_cff_ratio)) AS margin
-                 FROM tbl_inv_detail_mst AS A
-                 LEFT JOIN tbl_inv_mst AS B ON A.des_inv_no = B.inv_no
-                 LEFT JOIN tbl_order_dtn_detail_mst AS C ON A.des_det_uniq = C.det_uniq
-                 LEFT JOIN tbl_ffmc_reserve_order AS D ON C.det_ref_code = D.con_ref_code AND D.con_status = 'Picking'
-                 LEFT JOIN tbl_fg_inven_mst AS E ON D.con_res_pallet_id = E.pallet_id
-                 LEFT JOIN tbl_confirm_print_list AS F ON E.pallet_lot_no = F.list_conf_no
-                 LEFT JOIN tbl_bom_mst AS G ON E.pallet_bom_uniq = G.bom_uniq
-                 LEFT JOIN tbl_customer_mst AS H ON B.inv_cus_code = H.cus_code
-                 WHERE B.inv_date LIKE '$inc_month%' AND A.des_status IN('INV','Post Invoice') AND H.cus_type IN('B2B','GDJM') AND H.cus_sale_pic IN('GDJ00313','GDJ00310')
-                 GROUP BY inv_cus_code, cus_cff_ratio"
-            );
-            while($MIclResult = $MIcl->fetch(PDO::FETCH_ASSOC)){
-                array_push($json, $MIclResult);
+            foreach($column_name as $id=>$item){
+                if(trim($data->getCell($item . "3")->getValue()) != $column_head[$id]){
+                    echo json_encode(array('code'=>400, 'message'=>'หัว Column ' . $item . '3 ไม่ใช่ ' . $column_head[$id] . ' ตรวจสอบข้อมูลและดำเนินการใหม่อีกครั้ง'));
+                    return;
+                }   
             }
 
-            //todo >>>>>>>>>>>>>>>>>>>> Fetch sale list >>>>>>>>>>>>>>>>>>>>
-            //todo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            foreach($json as $id=>$item){
-                $SQList = '';
-                if($item['GroupCustomer'] == 'Included VMI'){
-                    $SQList = "SELECT user_code, user_name_en, user_position FROM tbl_user where user_dep_id = 'D011' AND user_enable = 1 AND user_position != 'Sale Executive'";
-                }else{
-                    $SQList = "SELECT user_code, user_name_en, user_position
-                               FROM tbl_user AS A 
-                               INNER JOIN tbl_customer_mst AS B ON A.user_code = B.cus_sale_pic
-                               WHERE B.cus_code = '".$item['GroupCustomer']."'";
-                }
+            echo count($data->getColumnIterator());
 
-                $normalList = $db_con->query($SQList);
-                while($normalResult = $normalList->fetch(PDO::FETCH_ASSOC)){
-                    $inc_rate = (($item['revenue'] - $item['cost']) / $item['cost']) * 100;
-                    $inc = $db_con->prepare("SELECT tiv_rate FROM IncentiveRates WHERE tiv_position = :position AND :incenrate BETWEEN tiv_min AND tiv_max ORDER BY tiv_uniq");
-                    $inc->bindParam(':position', $normalResult['user_position']);
-                    $inc->bindParam(':incenrate', $inc_rate);
-                    $inc->execute();
-                    $incResult = $inc->fetch(PDO::FETCH_ASSOC);
-                    
-                    $normalResult['inc_rate'] = $incResult['tiv_rate'];
-                    $normalResult['inc_total'] = $item['revenue'] * ($incResult['tiv_rate'] / 100);
-                    array_push($pril, $normalResult);
+            for($i=4;$i<=$highestRow;$i++){
+                $inv_period  = trim($data->getCell("A$i")->getValue());
+                $inv_no      = trim($data->getCell("B$i")->getValue());
+                $inv_date    = trim($data->getCell("C$i")->getValue());
+                $inv_type    = trim($data->getCell("D$i")->getValue());
+                $cus_name_en = trim($data->getCell("E$i")->getValue());
+                $total       = floatval(str_replace(",","", trim($data->getCell("F$i")->getValue())));
+                $vat         = floatval(str_replace(",","", trim($data->getCell("G$i")->getValue())));
+                $grand_total = floatval(str_replace(",","", trim($data->getCell("H$i")->getValue())));
+                $rv_no       = trim($data->getCell("I$i")->getValue());
+                $rv_date     = trim($data->getCell("J$i")->getValue());
+                $project     = trim($data->getCell("K$i")->getValue());
+                
+                if($inv_no != ''){
+                    $ivfile = $db_con->prepare("SELECT inv_no, inv_cus_code, inv_status FROM tbl_inv_mst WHERE inv_no = :inv_no");
+                    $ivfile->bindParam(':inv_no', $inv_no);
+                    $ivfile->execute();
+                    $ivResult = $ivfile->fetch(PDO::FETCH_ASSOC);
+
+                    if($ivResult['inv_no'] == ''){
+                        $vmfile = $vmi_con->query("SELECT inv_no, inv_cus_code, inv_status FROM tbl_inv_mst WHERE inv_no = '$inv_no'");
+                        $vmResult = $vmfile->fetch(PDO::FETCH_ASSOC);
+                        if($vmResult['inv_no'] == ''){
+                            echo json_encode(array('code'=>400, 'message'=>"ไม่พบข้อมูลเอกสาร $inv_no ทั้งบนระบบ MRP & VMI ตรวจสอบข้อมูลและดำเนินการอีกครั้ง"));
+                            $db_con = null;
+                            $vmi_con = null;
+                            return;
+                        }
+
+                        $ivd = $vmi_con->prepare(
+                            "SELECT SUM(det_amount) AS des_amount,
+                                    SUM(det_qty * bom_cost_per_pcs) AS des_cost,
+                                    inv_total
+                             FROM tbl_inv_detail_mst AS A
+                             LEFT JOIN tbl_inv_mst AS Main ON A.det_inv_no = Main.inv_no
+                             LEFT JOIN tbl_dn_usage_conf AS B ON A.det_dn_usage_id = B.dn_usage_id
+                             LEFT JOIN tbl_bom_mst AS C ON B.dn_bom_uniq = C.bom_uniq
+                             WHERE det_inv_no = '$inv_no'
+                             GROUP BY det_inv_no, inv_total"
+                        );
+                    }else{
+                        $ivd = $db_con->prepare(
+                            "SELECT SUM(des_amount) AS des_amount,
+                                    SUM(des_qty * cost_total_oh) AS des_cost,
+                                    inv_total
+                             FROM tbl_inv_detail_mst AS A
+                             LEFT JOIN tbl_inv_mst AS Main ON A.des_inv_no = Main.inv_no
+                             LEFT JOIN tbl_order_dtn_detail_mst AS B ON A.des_det_uniq = B.det_uniq
+                             LEFT JOIN tbl_bom_mst AS C ON B.det_bom_uniq = C.bom_uniq
+                             WHERE des_inv_no = '$inv_no'
+                             GROUP BY des_inv_no, inv_total"
+                        );
+                    }
+
+                    $ivd->execute();
+                    $ivdResult = $ivd->fetch(PDO::FETCH_ASSOC);
+
+                    if(number_format($ivdResult['inv_total'], 2) != number_format($total, 2)){
+                        echo json_encode(array('code'=>400, 'message'=>"ราคาขายของเอกสาร $inv_no ระหว่างไฟล์อัพโหลดและบนระบบไม่ตรงกัน ตรวจสอบข้อมูลและดำเนินการใหม่อีกครั้ง"));
+                        $db_con = null;
+                        return;
+                    }
+
+                    $revenue += $total;
+                    $amount_cost += $ivdResult['des_cost'];
                 }
             }
             
-            foreach ($pril as $id=>$item) {
-                $item['list'] = $id + 1;
-                $user_code = $item["user_code"];
-                $user_name_en = $item["user_name_en"];
-                $user_position = $item["user_position"];
-                $incTotal = $item["inc_total"];
-            
-                // ถ้ายังไม่มีข้อมูลสำหรับ user_name_en นี้ใน $result ให้เพิ่มเข้าไป
-                if (!isset($arr[$user_code])) {
-                    $arr[$user_code] = $item;
-                } else {
-                    // หากมีแล้วให้บวกค่า inc_total เข้ากับข้อมูลเดิม
-                    $arr[$user_code]["inc_total"] += $incTotal;
-                }
-            }
-
-
-            echo json_encode(array('code'=>200, 'message'=>'ok', 'datas'=>$json, 'prilla'=>array_values($arr)));
-            $db_con = null;
-            $vmi_con = null;
-            return;
+            echo "Revenue is $revenue\nAmount Cost 24% $amount_cost\nMargin " . ($revenue - $amount_cost) . "\nMargin % " . (($revenue - $amount_cost) / $revenue) ;
         } catch(Exception $e) {
             echo json_encode(array('code'=>400, 'message'=>'ไม่สามารถประมวลผลได้ ' . $e->getMessage()));
             $db_con = null;
-            $vmi_con = null;
             return;
         }
     }
